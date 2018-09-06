d22a6f5949900badd9cfb4e0fd091987f2edb5f9
eros <george.marr.cobc@gmail.com>
2018-09-06 22:40:55 +0100

    forgot to add that in

---
+++ plugin.c
@@ -0,0 +1,19 @@
+//
+// Created by george on 06/09/18.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "include/public_errors.h"
+#include "include/public_definitions.h"
+#include "include/public_errors_rare.h"
+#include "include/public_rare_definitions.h"
+#include "include/clientlib_publicdefinitions.h"
+#include "include/ts3_functions.h"
+
+static struct TS3Functions ts3Functions;
+
+#define _strcpy(dest, destSize, src) {
+
+}

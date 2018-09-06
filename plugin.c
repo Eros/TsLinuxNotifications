f0823e78445c65224ac8f3ce75857d4f24c2d1cb
eros <george.marr.cobc@gmail.com>
2018-09-06 22:44:09 +0100

    adds in some standard methods

--- plugin.c
+++ plugin.c
@@ -15,5 +15,42 @@
 static struct TS3Functions ts3Functions;
 
 #define _strcpy(dest, destSize, src) {
+    strncpy(dest, src, destSize - 1); \ (dest)[destSize - 1] = '\0';
+}
+#define PLUGIN_API_VERSION 22
 
+#define PATH_BUFSIZE 512
+#define COMMAND_BUFSIZE 128
+#define INFODATA_BUFSIZE 128
+#define SERVERINFO_BUFSIZE 256
+#define CHANNELINFO_BUFSIZE 512
+#define RETURNCODE_BUFSIZE 128
+
+static char* pluginID = NULL;
+
+int current_client(uint64 serverConnectionHandlerID, anyID other,
+                   anyID *client);
+
+int current_channel(uint64 serverConnectionHandlerID, uint64 other,
+                    uint64 *current);
+
+void send_server_message_notification(uint64 serverConnectionHandlerID,
+                                      const char *fromName, const char *message);
+
+void send_channel_message_notification(uint64 serverConnectionHandlerID,
+                                       anyID clientID, const char *fromName, const char *message);
+
+void show_welcome_message();
+
+const char* ts3plugin_name(){ return "TsLinuxNotifications"; }
+
+const char* ts3plugin_version(){ return "1.0-BETA"; }
+
+const int ts3plugin_apiVersion() { return PLUGIN_API_VERSION; }
+
+void shutdown(){
+    if(pluginID){
+        free(pluginID);
+        pluginID = NULL;
+    }
 }
